c5a5fdb36c4aa5be71cd7823d6a18d45496eb7b4
bunnycou <noahjcou@gmail.com>
2024-02-19 02:27:10 -0500

    added smash support and updated lots of code formatting

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # LoLOverpy
-Python based webserver for adding a very simple overlay for casting league of legends
+Python based webserver for adding a very simple overlay for casting league of legends or smash in OBS or other programs
 
 ## How to use
-### run `loloverlay.bat`
+### run `overpy.bat`
 - This will start the flask server on your pc
 
-### Add `http://127.0.0.1:5000/game` to your obs as a browser source
+### Add `http://127.0.0.1:5000/lolgame` or `http://127.0.0.1:5000/smash` to your obs as a browser source
 - Make sure to make the width and height 2000x2000 (just larger than 1920x1080)
-- `http://127.0.0.1:5000/game` is for in game, `http://127.0.0.1:500/draft` can be used for when in draft
+- `http://127.0.0.1:5000/lolgame` is for in game, `http://127.0.0.1:500/loldraft` can be used for when in League draft
 
-### Add a custom browser dock or open a browser and goto `http://127.0.0.1:5000/submit`
+### Add a custom browser dock or open a browser and goto `http://127.0.0.1:5000/lolsubmit` or `/smashsubmit`
 - This page allows you to edit the displayed content.
 
 ## Notes
--- app.py
+++ app.py
@@ -1,38 +1,102 @@
 from flask import Flask, render_template, request
+from math import ceil
 import time
 
 app = Flask(__name__)
 
-bt="UT"
-rt="UT"
-bs="0"
-rs="0"
-pn="14.1"
-gn="1"
+#LoL vars
+LoL_blue="UT"
+LoL_red="UT"
+LoL_blueScore="0"
+LoL_redScore="0"
+LoL_patch="14.1"
+LoL_game="1"
 
-@app.route("/game")
-def game():
-    # Path to your HTML file relative to the Python file
-    return render_template("overlay.html", btAbr=bt, rtAbr=rt, btScr=bs, rtScr=rs, patchno=pn, gameno=gn)
+#Smash Vars
+Smash_red="UT"
+Smash_blue="UT"
+Smash_redScore="0"
+Smash_blueScore="0"
+Smash_redStocks=12
+Smash_blueStocks=12
+Smash_redPlayerCount=""
+Smash_bluePlayerCount=""
 
-@app.route("/draft")
-def draft():
-    return render_template("draft.html", btAbr=bt, rtAbr=rt, btScr=bs, rtScr=rs, patchno=pn, gameno=gn)
+empty_circle = "○"
+filled_circle = "●"
 
-@app.route("/submit", methods=['Get','POST'])
-def submit():
-    global bt, rt, bs, rs, pn, gn
+#LoL Pages
+@app.route("/lolgame")
+def lolgame():
+    return render_template("lol_game.html", blue=LoL_blue, red=LoL_red, blueScore=LoL_blueScore, redScore=LoL_redScore, patch=LoL_patch, game=LoL_game)
+
+@app.route("/loldraft")
+def loldraft():
+    return render_template("lol_draft.html", blue=LoL_blue, red=LoL_red, blueScore=LoL_blueScore, redScore=LoL_redScore, patch=LoL_patch, game=LoL_game)
+
+@app.route("/lolsubmit", methods=['Get','POST'])
+def lolsubmit():
+    global LoL_blue, LoL_red, LoL_blueScore, LoL_redScore, LoL_patch, LoL_game
     if request.method == 'POST':
-        bt=request.form['btAbr']
-        rt=request.form['rtAbr']
-        bs=request.form['btScr']
-        rs=request.form['rtScr']
-        pn=request.form['patchno']
-        gn=request.form['gameno']
+        LoL_blue=request.form['blue']
+        LoL_red=request.form['red']
+        LoL_blueScore=request.form['blueScore']
+        LoL_redScore=request.form['redScore']
+        LoL_patch=request.form['patch']
+        LoL_game=request.form['game']
         swap = request.form.get("swap")
         if swap:
-            bt, rt = rt, bt
-            bs, rs = rs, bs
-        return render_template("submit.html", btAbr=bt, rtAbr=rt, btScr=bs, rtScr=rs, patchno=pn, gameno=gn, time="Updated at "+time.strftime("%H:%M:%S"))
+            LoL_blue, LoL_red = LoL_red, LoL_blue
+            LoL_blueScore, LoL_redScore = LoL_redScore, LoL_blueScore
+        return render_template("lol_submit.html", blue=LoL_blue, red=LoL_red, blueScore=LoL_blueScore, redScore=LoL_redScore, patch=LoL_patch, game=LoL_game, time="Updated at "+time.strftime("%H:%M:%S"))
     else:
-        return render_template("submit.html", btAbr=bt, rtAbr=rt, btScr=bs, rtScr=rs, patchno=pn, gameno=gn, time="")
+        return render_template("lol_submit.html", blue=LoL_blue, red=LoL_red, blueScore=LoL_blueScore, redScore=LoL_redScore, patch=LoL_patch, game=LoL_game, time="")
+    
+#Smash Pages
+@app.route("/smash")
+def smash():
+    Smash_redPlayerCount = redPlayerCt()
+    Smash_bluePlayerCount = bluePlayerCt()
+    return render_template("smash_game.html", blue=Smash_blue, red=Smash_red, blueScore=Smash_blueScore, redScore=Smash_redScore, blueStocks=Smash_blueStocks, redStocks=Smash_redStocks, bluePlayerCount=Smash_bluePlayerCount, redPlayerCount=Smash_redPlayerCount)
+
+def redPlayerCt():
+    ret = ""
+    players = playerCt(Smash_redStocks)
+    elim = 4-players
+    for i in range(players):
+        ret += filled_circle + " "
+    for i in range(elim):
+        ret += empty_circle + " "
+    return ret.strip()
+
+def bluePlayerCt():
+    ret = ""
+    players = playerCt(Smash_blueStocks)
+    elim = 4-players
+    for i in range(elim):
+        ret += empty_circle + " "
+    for i in range(players):
+        ret += filled_circle + " "
+    return ret.strip()
+
+def playerCt(stocks):
+    return ceil(stocks/3)
+
+@app.route("/smashsubmit", methods=['Get','POST'])
+def smashsubmit():
+    global Smash_red, Smash_redScore, Smash_redStocks, Smash_blue, Smash_blueScore, Smash_blueStocks
+    if request.method == "POST":
+        Smash_red = request.form["red"]
+        Smash_redScore = request.form["redScore"]
+        Smash_redStocks = int(request.form["redStocks"])
+        Smash_blue = request.form["blue"]
+        Smash_blueScore = request.form["blueScore"]
+        Smash_blueStocks = int(request.form["blueStocks"])
+        swap = request.form.get("swap")
+        if swap:
+            Smash_red, Smash_blue = Smash_blue, Smash_red
+            Smash_redScore, Smash_blueScore = Smash_blueScore, Smash_redScore
+            Smash_redStocks, Smash_blueStocks = Smash_blueStocks, Smash_redStocks
+        return render_template("smash_submit.html", red=Smash_red, redScore=Smash_redScore, redStocks=Smash_redStocks, blue=Smash_blue, blueScore=Smash_blueScore, blueStocks=Smash_blueStocks, time="Updated at "+time.strftime("%H:%M:%S"))
+    else:
+        return render_template("smash_submit.html", red=Smash_red, redScore=Smash_redScore, redStocks=Smash_redStocks, blue=Smash_blue, blueScore=Smash_blueScore, blueStocks=Smash_blueStocks, time="")
